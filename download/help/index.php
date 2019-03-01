2a6436a3ed6602f35f80fc8f4ce277a5c5d7c35b
sizious <sizious@gmail.com>
2019-03-01 16:54:08 +0100

    Updating the website content.

--- download/help/index.php
+++
@@ -1,7 +0,0 @@
-<?php 		
-	$filename = "./release.txt";
-	$handle = fopen($filename, "r");
-	$release_filename = fread($handle, filesize($filename));
-	fclose($handle);	
-	header("Location: http://dreamsdk.sizious.com/$release_filename");	
-?>
